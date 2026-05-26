17ce4d8198175478c8d614074b73c08d727a42b7
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-05-26 11:44:57 -0400

    Add BasicItemModelHolder#textureDonor

--- src/main/java/tamaized/datagenutil/assets/bakedmodel/item/BasicItemModelHolder.java
+++ src/main/java/tamaized/datagenutil/assets/bakedmodel/item/BasicItemModelHolder.java
@@ -10,17 +10,27 @@ import tamaized.datagenutil.assets.bakedmodel.ExtendedTextureMapping;
 import tamaized.datagenutil.assets.bakedmodel.FurtherExtendedModelTemplateBuilder;
 import tamaized.pkginfoutil.PublicApi;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 @PublicApi
 public abstract class BasicItemModelHolder extends ItemModelHolder {
 
 	@Override
 	protected abstract DeferredHolder<Item, ? extends Item> itemForName();
 
+	protected Optional<Supplier<Item>> textureDonor() {
+		return Optional.empty();
+	}
+
 	protected abstract ModelTemplate template();
 
 	@Override
 	public Identifier finalize(ItemModelGenerators provider, FurtherExtendedModelTemplateBuilder model) {
-		provider.generateFlatItem(itemForName().value(), template());
+		textureDonor().ifPresentOrElse(
+			donor -> provider.generateFlatItem(itemForName().value(), donor.get(), template()),
+			() -> provider.generateFlatItem(itemForName().value(), template())
+		);
 		return ModelLocationUtils.getModelLocation(itemForName().value());
 	}
 
