fe436926d2848d38d1f0ec44b3c16f38d108faa2
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-08-23 09:13:13 -0400

    Add HolderGetter<Item> param to RecipeHolder

--- src/main/java/tamaized/datagenutil/data/recipe/RecipeHolder.java
+++ src/main/java/tamaized/datagenutil/data/recipe/RecipeHolder.java
@@ -1,10 +1,15 @@
 package tamaized.datagenutil.data.recipe;
 
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.world.item.Item;
+import tamaized.pkginfoutil.PublicApi;
 
+@PublicApi
 public abstract class RecipeHolder {
 
-	public abstract void make(HolderLookup.Provider provider, RecipeOutput output);
+	@PublicApi
+	public abstract void make(HolderLookup.Provider provider, HolderGetter<Item> itemProvider, RecipeOutput output);
 
 }
